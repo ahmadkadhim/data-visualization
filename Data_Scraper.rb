919acf76329cdfcea2274f91b51f8e73e3f22218
MarissaMac <marissa.maclellan@gmail.com>
2013-06-13 14:21:53 -0400

    cleaned text being written to file

--- Data_Scraper.rb
+++ Data_Scraper.rb
@@ -8,10 +8,10 @@ artists = []
 relations = []
 
 File.open('artists.txt', 'w') do |f|
-	for i in (1..4)
+	for i in (1..2)
 		page = Nokogiri::HTML(open("http://www.last.fm/#{artist_list[counter]}"))
 		bloated_artists = page.css('a.similar-artist')
-		relations << counter
+		relations << counter.to_s
 		relations << "Artist:" + artist_list[counter] + "\n"
 		for j in (0..(bloated_artists.length-1))
 			artists << bloated_artists[j]['href']
@@ -25,8 +25,14 @@ File.open('artists.txt', 'w') do |f|
 		end 
 		counter +=1
 	end
+
+	relations.each do |x|
+		x.gsub!(/[\+]/," ")
+		x.gsub!(/\/[a-zA-z]*\//,"")
+	end
+	
 	f.write("\n\n\n\n Relations\n")
-	f.write("#{relations}")
+	f.write("#{relations}\n")
 end
 
 
