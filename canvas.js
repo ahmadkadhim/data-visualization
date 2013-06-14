8c8da1d9b592310efa7bd704db621eea877743c9
MarissaMac <marissa.maclellan@gmail.com>
2013-06-13 20:19:01 -0400

    placed images on canvas

--- canvas.js
+++ canvas.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 	
-
-
 //Drawing the canvas of related artists with images
 
 var canvasElement;
@@ -10,10 +8,12 @@ var pattern;
 
 var canvasWidth = 650;
 var canvasHeight = 650;
-var fitFactor = 300;
+var fitFactor = 180;
 var widthFactor = canvasWidth/fitFactor;
 var heightFactor = canvasHeight/fitFactor;
 var hexagonBoxSide = 100*widthFactor;
+
+console.log(startingArtist)
 
 var startingArtist = $("#0link").text();
 var firstArtist = $("#1link").text();
@@ -23,13 +23,23 @@ var fourthArtist = $("#4link").text();
 var fifthArtist = $("#5link").text();
 var sixthArtist = $("#6link").text();
 
-var startingImage = $("#0image");
-var firstImage = $("#1image");
-var secondImage = $("2image");
-var thirdImage = $("#3image");
-var fourthImage = $("#4image");
-var fifthImage = $("#5image");
-var sixthImage = $("#6image");
+console.log(startingArtist);
+
+//var startingImage = $("#0image");
+var startingImage = new Image();
+startingImage.src = $("#0image").attr("src");
+var firstImage = new Image();
+firstImage.src = $("#1image").attr("src");
+var secondImage = new Image();
+secondImage.src = $("#2image").attr("src");
+var thirdImage = new Image();
+thirdImage.src = $("#3image").attr("src");
+var fourthImage = new Image();
+fourthImage.src = $("#4image").attr("src");
+var fifthImage = new Image();
+fifthImage.src = $("#5image").attr("src");
+var sixthImage = new Image();
+sixthImage.src = $("#6image").attr("src");
 
 var imageWidth = 100;
 var imageHeight = 100;
@@ -49,7 +59,7 @@ var fifthPointY = (canvasHeight-hexagonBoxSide)/2+(hexagonBoxSide*.21325)
 var sixthPointX = canvasWidth/2;
 var sixthPointY = (canvasHeight-hexagonBoxSide)/2
 
-var offset = 30;
+var offset = 20;
 
 runner();
 
@@ -60,6 +70,19 @@ function addCanvas () {
 	canvasElement.width = canvasWidth;
 	canvasElement.height = canvasHeight;
 	context = canvasElement.getContext("2d");
+
+	// Possible feature to make the whole canvas circular shape
+
+	// context.fillStyle = "rgba(cc,cc,33,.20)";
+	// // Begin path
+	// context.beginPath();
+	// //Canvas shape
+	// context.arc(400,400,300,0,Math.PI*2,true);
+	// // Close path
+	// context.closePath();
+	// // Fill shape
+	// context.fill();
+
 }
 
 function drawGrid () {
@@ -105,17 +128,20 @@ function drawHexagon () {
 
 function writeText (artist, x, y) {
 
-	context.font = "20px Avant Garde"
+	context.font = "24px Avant Garde"
+	context.fillStyle = "#777"
 	context.textBaseline = "middle";
 	context.textAlign = "center";
-	context.fillText(artist, x, y)
+	context.fillText(artist, x, y+offset)
 }
 
 function runner () {
 	addCanvas();
 	drawGrid();
 	drawHexagon();
-	// context.drawImage(startingImage, 0, 0);
+}
+
+	//context.drawImage(startingImage, 20, 40);
 	// context.drawImage(firstImage, 0, 0);
 	// context.drawImage(secondImage, 0, 0);
 	// context.drawImage(thirdImage, 0, 0);
@@ -123,13 +149,87 @@ function runner () {
 	// context.drawImage(fifthImage, 0, 0);
 	// context.drawImage(sixthImage, 0, 0);
 
-	writeText(startingArtist, startingPointX,startingPointY);
-	writeText(firstArtist, firstPointX-offset, firstPointY-offset);
+CanvasRenderingContext2D.prototype.roundRect=function(x,y,width,height,tl,tr,br,bl) {
+    var x1,x2,x3,x4,y1,y2,y3,y4,radii,ratio=0,CURVE2KAPPA=0.5522847498307934;
+    ratio=Math.min(width/(tl+tr),width/(br+bl));
+    if ((ratio>0)&&(ratio<1)) {
+        tl*=ratio;
+        tr*=ratio;
+        bl*=ratio;
+        br*=ratio;
+    }
+    xw=x+width;
+    yh=y+height;
+    x1=x+tl;
+    x2=xw-tr;
+    x3=xw-br;
+    x4=x+bl;
+    y1=y+tr;
+    y2=yh-br;
+    y3=yh-bl;
+    y4=y+tl;
+    this.beginPath();
+    this.moveTo(x1,y);
+    this.lineTo(x2,y);
+    radii=CURVE2KAPPA*tr;
+    this.bezierCurveTo(x2+radii,y,xw,y1-radii,xw,y1);
+    this.lineTo(xw,y2);
+    radii=CURVE2KAPPA*br;
+    this.bezierCurveTo(xw,y2+radii,x3+radii,yh,x3,yh);
+    this.lineTo(x4,yh);
+    radii=CURVE2KAPPA*bl;
+    this.bezierCurveTo(x4-radii,yh,x,y3+radii,x,y3);
+    this.lineTo(x,y4);
+    radii=CURVE2KAPPA*tl;
+    this.bezierCurveTo(x,y4-radii,x1-radii,y,x1,y);
+    this.stroke();
+}
+// var ctx=document.getElementById("canvas").getContext("2d");
+// ctx.roundRect(10.5,10.5,50,50,5,5,10,15);
+// ctx.strokeStyle="red";
+// ctx.roundRect(80.5,10.5,50,50,5000,500,100,150);
+// ctx.strokeStyle="blue";
+// ctx.roundRect(160.5,10.5,50,50,2500,250,50,75);
+
+
+
+	startingImage.onload = function() {
+		context.drawImage(startingImage, startingPointX-(20*widthFactor), startingPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
+		writeText(startingArtist, startingPointX, startingPointY);
+	}
+	firstImage.onload = function() {
+		context.drawImage(firstImage, firstPointX-(20*widthFactor), firstPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
+		writeText(firstArtist, firstPointX-offset, firstPointY-offset);
+	}
+	secondImage.onload = function() {
+		context.drawImage(secondImage, secondPointX-(20*widthFactor), secondPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
+		writeText(secondArtist, secondPointX-offset, secondPointY-offset);
+	}
+	thirdImage.onload = function() {
+		context.drawImage(thirdImage, thirdPointX-(20*widthFactor), thirdPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
+		writeText(thirdArtist, thirdPointX-offset, thirdPointY-offset);
+	}
+	fourthImage.onload = function() {
+		context.drawImage(fourthImage, fourthPointX-(20*widthFactor), fourthPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
+		writeText(fourthArtist, fourthPointX-offset, fourthPointY-offset);
+	}
+	fifthImage.onload = function() {
+		context.drawImage(fifthImage, fifthPointX-(20*widthFactor), fifthPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
+		writeText(fifthArtist, fifthPointX-offset, fifthPointY-offset);
+	}
+	sixthImage.onload = function() {
+		context.drawImage(sixthImage, sixthPointX-(20*widthFactor), sixthPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
+		writeText(sixthArtist, sixthPointX-offset, sixthPointY-offset);
+	}
+
+	// writeText(startingArtist, startingPointX,startingPointY);
+	// writeText(firstArtist, firstPointX-offset, firstPointY-offset);
 	writeText(secondArtist, secondPointX-offset, secondPointY+offset);
 	writeText(thirdArtist, thirdPointX, thirdPointY+offset);
 	writeText(fourthArtist, fourthPointX+offset, fourthPointY+offset);
 	writeText(fifthArtist, fifthPointX+offset, fifthPointY-offset);
 	writeText(sixthArtist, sixthPointX, sixthPointY-offset);
-}
 
 })
+
+
--- get_divs_for_html.rb
+++ get_divs_for_html.rb
@@ -9,14 +9,25 @@ File.open('divs_for_html.html','w') do |html|
 			array << line.chomp
 		end
 	end
-	
+	html.write("<!doctype html>
+	<html> \n
+	<head> \n
+	<title>Canvas Experiment</title>
+	<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js'></script>\n
+	<script src='canvas.js'></script>\n
+	<link rel ='stylesheet' href='canvas.css' /> \n
+	</head>\n
+	<body>\n")
 	for i in (1..14)
 		if i%2 !=0
-		html.write("<div id='#{(i-1)/2}image' class='artists'>")
+		html.write("<div id='#{(i-1)/2}link' class='artists' style='color:white;' >")
 		html.write("#{array[i-1]}")
 		html.write("</div>\n")
 		else
-		html.write("<img id='#{(i-1)/2}link' class='link' src='#{array[i-1]}'/>\n")
+		html.write("<img id='#{(i-1)/2}image' class='image' src='#{array[i-1]}'/>\n")
 		end
 	end
+	html.write("
+	</body>\n
+	</html>")
 end
