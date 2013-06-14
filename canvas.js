e91cadfb209f4a2da032089e03189b7307230390
AhmadKadhim <akadhim13@gmail.com>
2013-06-13 22:53:01 -0400

    Canvas arcs to clip images into circular shapes, simple color and shadow styling

--- canvas.js
+++ canvas.js
@@ -6,8 +6,8 @@ var canvasElement;
 var drawingContext;
 var pattern;
 
-var canvasWidth = 650;
-var canvasHeight = 650;
+var canvasWidth = 900;
+var canvasHeight = 900;
 var fitFactor = 180;
 var widthFactor = canvasWidth/fitFactor;
 var heightFactor = canvasHeight/fitFactor;
@@ -59,7 +59,7 @@ var fifthPointY = (canvasHeight-hexagonBoxSide)/2+(hexagonBoxSide*.21325)
 var sixthPointX = canvasWidth/2;
 var sixthPointY = (canvasHeight-hexagonBoxSide)/2
 
-var offset = 20;
+var offset = 0;
 
 runner();
 
@@ -70,19 +70,12 @@ function addCanvas () {
 	canvasElement.width = canvasWidth;
 	canvasElement.height = canvasHeight;
 	context = canvasElement.getContext("2d");
-
-	// Possible feature to make the whole canvas circular shape
-
-	// context.fillStyle = "rgba(cc,cc,33,.20)";
-	// // Begin path
-	// context.beginPath();
-	// //Canvas shape
-	// context.arc(400,400,300,0,Math.PI*2,true);
-	// // Close path
-	// context.closePath();
-	// // Fill shape
-	// context.fill();
-
+	var centerX = canvasWidth/2;
+	var centerY = canvasHeight/2;
+	context.beginPath();
+	context.arc(centerX,centerY,400,0,Math.PI*2,false);
+	context.fillStyle = "#999";
+	context.fill();	
 }
 
 function drawGrid () {
@@ -122,17 +115,34 @@ function drawHexagon () {
 	context.moveTo(sixthPointX,sixthPointY);
 	context.lineTo(firstPointX,firstPointY);
 	context.lineWidth = 4;
-	context.strokeStyle = "#000";
+	context.strokeStyle = "#ccc";
 	context.stroke();
+	context.closePath();
 }
 
 function writeText (artist, x, y) {
 
-	context.font = "24px Avant Garde"
-	context.fillStyle = "#777"
+	context.font = "22px Helvetica Neue, Arial";
+	if(artist.length < 6) {
+		context.font = "32px Helvetica Neue, Arial";
+	}
+	if(artist.length < 12) {
+		context.font = "28px Helvetica Neue, Arial";
+	}
+	if(artist.length > 20) {
+		context.font = "16px Helvetica Neue, Arial";
+	}
 	context.textBaseline = "middle";
 	context.textAlign = "center";
+	context.fillStyle = "#FFF";
+	context.fillText(artist,x+1,y+offset+1);
+	context.shadowColor = "#222";
+	context.shadowOffsetX = 2;
+	context.shadowOffsetY = 2;
+	context.shadowBlur = 5;
+	context.fillStyle = "#F6F4F4";
 	context.fillText(artist, x, y+offset)
+
 }
 
 function runner () {
@@ -149,77 +159,92 @@ function runner () {
 	// context.drawImage(fifthImage, 0, 0);
 	// context.drawImage(sixthImage, 0, 0);
 
-CanvasRenderingContext2D.prototype.roundRect=function(x,y,width,height,tl,tr,br,bl) {
-    var x1,x2,x3,x4,y1,y2,y3,y4,radii,ratio=0,CURVE2KAPPA=0.5522847498307934;
-    ratio=Math.min(width/(tl+tr),width/(br+bl));
-    if ((ratio>0)&&(ratio<1)) {
-        tl*=ratio;
-        tr*=ratio;
-        bl*=ratio;
-        br*=ratio;
-    }
-    xw=x+width;
-    yh=y+height;
-    x1=x+tl;
-    x2=xw-tr;
-    x3=xw-br;
-    x4=x+bl;
-    y1=y+tr;
-    y2=yh-br;
-    y3=yh-bl;
-    y4=y+tl;
-    this.beginPath();
-    this.moveTo(x1,y);
-    this.lineTo(x2,y);
-    radii=CURVE2KAPPA*tr;
-    this.bezierCurveTo(x2+radii,y,xw,y1-radii,xw,y1);
-    this.lineTo(xw,y2);
-    radii=CURVE2KAPPA*br;
-    this.bezierCurveTo(xw,y2+radii,x3+radii,yh,x3,yh);
-    this.lineTo(x4,yh);
-    radii=CURVE2KAPPA*bl;
-    this.bezierCurveTo(x4-radii,yh,x,y3+radii,x,y3);
-    this.lineTo(x,y4);
-    radii=CURVE2KAPPA*tl;
-    this.bezierCurveTo(x,y4-radii,x1-radii,y,x1,y);
-    this.stroke();
-}
-// var ctx=document.getElementById("canvas").getContext("2d");
-// ctx.roundRect(10.5,10.5,50,50,5,5,10,15);
-// ctx.strokeStyle="red";
-// ctx.roundRect(80.5,10.5,50,50,5000,500,100,150);
-// ctx.strokeStyle="blue";
-// ctx.roundRect(160.5,10.5,50,50,2500,250,50,75);
 
-
+	context.save()
+	context.strokeStyle = "#475c78"
+	context.lineWidth = 8
 
 	startingImage.onload = function() {
+		context.save();
+		context.arc(startingPointX,startingPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.clip();
 		context.drawImage(startingImage, startingPointX-(20*widthFactor), startingPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
 		writeText(startingArtist, startingPointX, startingPointY);
+		context.restore();
+		context.shadowColor = "#CCC";
+		context.shadowOffsetX = 0;
+		context.shadowOffsetY = 0;
+		context.shadowBlur = 4;
+		context.beginPath();
+		context.arc(startingPointX,startingPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.stroke();
 	}
+
 	firstImage.onload = function() {
+		context.save();
+		context.arc(firstPointX,firstPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.clip();
 		context.drawImage(firstImage, firstPointX-(20*widthFactor), firstPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
 		writeText(firstArtist, firstPointX-offset, firstPointY-offset);
+		context.restore();
+		context.beginPath();
+		context.arc(firstPointX,firstPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.stroke();
 	}
 	secondImage.onload = function() {
+		context.save();
+		context.arc(secondPointX,secondPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.clip();
 		context.drawImage(secondImage, secondPointX-(20*widthFactor), secondPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
 		writeText(secondArtist, secondPointX-offset, secondPointY-offset);
+		context.restore();
+		context.beginPath();
+		context.arc(secondPointX,secondPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.stroke();
 	}
 	thirdImage.onload = function() {
+		context.save();
+		context.arc(thirdPointX,thirdPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.clip();
 		context.drawImage(thirdImage, thirdPointX-(20*widthFactor), thirdPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
 		writeText(thirdArtist, thirdPointX-offset, thirdPointY-offset);
+		context.restore();
+		context.beginPath();
+		context.arc(thirdPointX,thirdPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.stroke();
 	}
 	fourthImage.onload = function() {
+		context.save();
+		context.arc(fourthPointX,fourthPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.clip();
 		context.drawImage(fourthImage, fourthPointX-(20*widthFactor), fourthPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
 		writeText(fourthArtist, fourthPointX-offset, fourthPointY-offset);
+		context.restore();
+		context.beginPath();
+		context.arc(fourthPointX,fourthPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.stroke();
 	}
 	fifthImage.onload = function() {
+		context.save();
+		context.arc(fifthPointX,fifthPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.clip();
 		context.drawImage(fifthImage, fifthPointX-(20*widthFactor), fifthPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
 		writeText(fifthArtist, fifthPointX-offset, fifthPointY-offset);
+		context.restore();
+		context.beginPath();
+		context.arc(fifthPointX,fifthPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.stroke();
 	}
 	sixthImage.onload = function() {
+		context.save();
+		context.arc(sixthPointX,sixthPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.clip();
 		context.drawImage(sixthImage, sixthPointX-(20*widthFactor), sixthPointY-(20*heightFactor),40*widthFactor,40*heightFactor);
 		writeText(sixthArtist, sixthPointX-offset, sixthPointY-offset);
+		context.restore();
+		context.beginPath();
+		context.arc(sixthPointX,sixthPointY,(40*heightFactor)/2,0,Math.PI*2,true);
+		context.stroke();
 	}
 
 	// writeText(startingArtist, startingPointX,startingPointY);
